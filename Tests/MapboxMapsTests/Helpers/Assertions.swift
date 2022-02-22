6e344183b80b75bbe69de86ac009338b6db1d853
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-02-22 16:28:17 +0200

    Add assertParameter...Identical

--- Tests/MapboxMapsTests/Helpers/Assertions.swift
+++ Tests/MapboxMapsTests/Helpers/Assertions.swift
@@ -1,17 +1,6 @@
 import Foundation
 import XCTest
 
-func assertFirstCallParameterEqual<P, R, T>(
-    _ methodStub: Stub<P, R>,
-    _ value: T,
-    for keyPath: KeyPath<P, T>,
-    _ message: String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T: Equatable {
-        XCTAssertFalse(methodStub.parameters.isEmpty, message, file: file, line: line)
-        XCTAssertEqual(methodStub.parameters.first?[keyPath: keyPath], value, message, file: file, line: line)
-    }
-
 func assertFirstCallParameterEqual<P, R>(
     _ methodStub: Stub<P, R>,
     _ firstParameter: P,
@@ -39,6 +28,35 @@ func assertCallParametersEqual<P, R>(
     file: StaticString = #file,
     line: UInt = #line) where P: Equatable {
         XCTAssertEqual(methodStub.parameters, parameters, message, file: file, line: line)
+    }
+
+func assertFirstCallParameterIdentical<P, R>(
+    _ methodStub: Stub<P, R>,
+    _ firstParameter: P,
+    _ message: String = "",
+    file: StaticString = #file,
+    line: UInt = #line) where P: AnyObject {
+        XCTAssertFalse(methodStub.parameters.isEmpty, message, file: file, line: line)
+        XCTAssertIdentical(methodStub.parameters.first, firstParameter, message, file: file, line: line)
+    }
+
+func assertLastCallParameterIdentical<P, R>(
+    _ methodStub: Stub<P, R>,
+    _ firstParameter: P,
+    _ message: String = "",
+    file: StaticString = #file,
+    line: UInt = #line) where P: AnyObject {
+        XCTAssertFalse(methodStub.parameters.isEmpty, message, file: file, line: line)
+        XCTAssertIdentical(methodStub.parameters.last, firstParameter, message, file: file, line: line)
+    }
+
+func assertCallParametersIdentical<P, R>(
+    _ methodStub: Stub<P, R>,
+    _ parameters: P...,
+    message: String = "",
+    file: StaticString = #file,
+    line: UInt = #line) where P: AnyObject {
+        XCTAssertIdentical(methodStub.parameters as AnyObject, parameters, message, file: file, line: line)
     }
 
 func assertMethodCall<P, R>(
